5ed29c68df727dfcc10167db8d0313be74172303
Maicon Pereira <maicond2701@gmail.com>
2026-05-20 15:59:28 -0300

    Adicionando nova alteracao

--- src/server.js
+++ src/server.js
@@ -18,11 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas ao AllBooks. Acesse essa API em http://localhost:8000")
-=======
-  console.log("Boas-vindas ao AllBooks")
-  console.log("Essa é uma API de uma loja de livros online")
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 2c578ede116fba4232d32a80d95c9784b7c553c4
+  console.log("Boas-vindas ao AllBooks. Opa acesse essa API em http://localhost:8000")
 })
